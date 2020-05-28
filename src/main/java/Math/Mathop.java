6c9a8d046e4e18636602ae792adcf4a6aa74cc45
FunnyLegend <58954723+FunnyLegend@users.noreply.github.com>
2020-05-28 08:58:16 +0300

    Initial commit

---
+++ src/main/java/Math/Mathop.java
@@ -0,0 +1,20 @@
+package Math;
+
+public class Mathop {
+    public double add(double firstNum, double secondNum){
+        return firstNum + secondNum;
+    }
+
+    public double sub(double firstNum, double secondNum){
+        return firstNum - secondNum;
+    }
+
+    public double mul(double firstNum, double secondNum){
+        return firstNum * secondNum;
+    }
+
+    public double div(double firstNum, double secondNum){
+        return (double) firstNum / secondNum;
+    }
+
+}
